10eb391a95933b3e63ce61a0d9625229f2e68d69
rdelvallej32 <rdelvallej@gmail.com>
2016-06-06 13:19:05 -0400

    Fix show action
    - Show was not working because Topics are not going to be created by the user

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -13,7 +13,7 @@ class TopicsController < ProtectedController
   # GET /topics/1
   # GET /topics/1.json
   def show
-    render json: @topic
+    render json: Topic.find(params[:id])
   end
 
   def randomshow
--- scripts/rating.sh
+++ scripts/rating.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 TOKEN=BAhJIiU3MmJkNmM2OTY2ZTlhZWIzODkzNzBhZDI2ZTQ2OTkzMQY6BkVG--1637fd9c9f69fd473f72b0303d0f5a5ab16f6105
 
-curl --include --request GET http://localhost:3000/ratings/1 \
+curl --include --request GET http://localhost:3000/ratings \
   --header "Authorization: Token token=$TOKEN"
